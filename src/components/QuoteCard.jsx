2c7f0ae2f5eacabc99d660b0a6c07239b5d0ead0
Jhener Daniel <jhener990@gmail.com>
2022-07-26 17:44:05 -0500

    frame motion

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
-import Button from './Button'
+import React from "react";
+import Button from "./Button";
+import { motion, AnimatePresence } from "framer-motion";
 
-const QuoteCard = ({index, nextRandom, colorRandom}) => {
+const variants = {
+  hidden: {
+    opacity: 0,
+    y: -10,
+    scale: 0.99
+  },
+  visible: {
+    opacity: 1,
+    y:0,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const QuoteCard = ({ index, nextRandom, colorRandom }) => {
   return (
-    <article className='quote'>
-        <p className='quote-text' >"{index.quote}"</p>
-        <p className='quote-author' >{index.author}</p>
-        <Button 
-        nextRandom={nextRandom}
-        colorRandom={colorRandom}
-        />
-    </article>
-  )
-}
+    
+      <motion.article
+        className="quote"
+        key={index.author}
+        variants={variants}
+        initial="hidden"
+        animate="visible"
+        layout
+      >
+        <p className="quote-text">"{index.quote}"</p>
+        <p className="quote-author">{index.author}</p>
+        <Button nextRandom={nextRandom} colorRandom={colorRandom} />
+      </motion.article>
+  );
+};
 
-export default QuoteCard
+export default QuoteCard;
